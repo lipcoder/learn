1d65efbef2f68936b16dcd39bedb38ea176ae49f
lipcoder <lipcoder@foxmail.com>
2026-01-04 16:56:42 +0800

    how to use go mod

---
+++ temp/geeting/geeting.go
@@ -0,0 +1,11 @@
+package geeting
+
+import "fmt"
+
+func Hello(){
+	fmt.Print("hello ")
+}
+
+func Hi (){
+	fmt.Println("hi")
+}
---
+++ temp/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"lipcoder/bil"
+	"lipcoder/geeting"
+)
+
+func main() {
+    geeting.Hello()
+    bil.World()
+}
