e681582c9ab4f2377dafa013ffac878b9901996b
lipcoder <lipcoder@foxmail.com>
2026-01-08 22:16:59 +0800

    nothing

--- go/2026.1.5.for..range/main.go
+++ go/2026.1.5.for..range/main.go
@@ -1,24 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main(){
-	notes := [7]string{"do","re","mi","fa","so","la","xi"}
+func main() {
+	
+	notes := [7]string{"do", "re", "mi", "fa", "so", "la", "xi"}
 
 	// 输出数组里面的 for循环1
-	fmt.Println("len(notes)=",len(notes))
-	for i:=0 ;i<len(notes);i++{
+	fmt.Println("len(notes)=", len(notes))
+	for i := 0; i < len(notes); i++ {
 		v := notes[i]
-		fmt.Println(i,v)
+		fmt.Print(i, v)
+		fmt.Print(" ")
 	}
+	fmt.Println()
 
 	// 本质上就是把上面这个写法糖成下面这样，只不过是将这个控制输出范围交给了编译器
-	// 输出数组里面的 for range
-	for index, note := range notes{
-		fmt.Println(index,note)
+	// range --> 输出右边这个数组/切片的索引和值
+	for index, note := range notes {
+		fmt.Print(index, note)
+		fmt.Print(" ")
 	}
+	fmt.Println()
 
-	for _,note1 := range notes{
-		fmt.Println(note1)
+	for _, note1 := range notes {
+		fmt.Print(note1)
+		fmt.Print(" ")
 	}
 }
