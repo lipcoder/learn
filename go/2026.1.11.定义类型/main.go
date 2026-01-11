7e7557cb4d2a47c027a05ca13e6aa5909b416a97
lipcoder <lipcoder@foxmail.com>
2026-01-11 19:37:18 +0800

    定义方法

---
+++ go/2026.1.11.定义类型/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type MyType string
+type Money float64
+type Yuan float64
+type Dollar float64
+
+func NewMyType() MyType {
+	var s, sep string
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = ""
+	}
+	return MyType(s)
+}
+
+// sayHi定义在MyType上
+func (m MyType) sayHi() {
+	fmt.Println("hi", m)
+}
+
+func (g Dollar)ChangeMoney() Yuan {
+	return Yuan(g*7)
+}
+
+func (g Yuan)ChangeMoney() Dollar{
+	return Dollar(g/7)
+}
+
+func main() {
+	value := NewMyType()
+	value.sayHi()
+
+	money := Yuan(100)
+	china := money.ChangeMoney()
+	fmt.Println(china)
+}
