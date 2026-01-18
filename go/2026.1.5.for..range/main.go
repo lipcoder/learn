f1e8a7fe1e8d3694ec0e6b53e304fc133d59b474
lipcoder <lipcoder@foxmail.com>
2026-01-18 18:59:01 +0800

    温故而知新

--- go/2026.1.5.for..range/main.go
+++ go/2026.1.5.for..range/main.go
@@ -5,28 +5,25 @@ import (
 )
 
 func main() {
-	
+
 	notes := [7]string{"do", "re", "mi", "fa", "so", "la", "xi"}
 
 	// 输出数组里面的 for循环1
 	fmt.Println("len(notes)=", len(notes))
 	for i := 0; i < len(notes); i++ {
 		v := notes[i]
-		fmt.Print(i, v)
-		fmt.Print(" ")
+		fmt.Print(i, v, " ")
 	}
 	fmt.Println()
 
 	// 本质上就是把上面这个写法糖成下面这样，只不过是将这个控制输出范围交给了编译器
-	// range --> 输出右边这个数组/切片的索引和值
+	// range 输出右边这个数组/切片的索引和值
 	for index, note := range notes {
-		fmt.Print(index, note)
-		fmt.Print(" ")
+		fmt.Print(index, note, " ")
 	}
 	fmt.Println()
 
 	for _, note1 := range notes {
-		fmt.Print(note1)
-		fmt.Print(" ")
+		fmt.Print(note1," ")
 	}
 }
