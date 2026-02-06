8bb7454f21b8d570ce631a100cf728222b6183e1
lipcoder <lipcoder@foxmail.com>
2026-02-06 20:24:05 +0800

    recover和panic,虽然创始人都不推荐使用

---
+++ go/2026.2.6.recover与panic/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func A() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("recover接住panic,panic内容为:", r)
+		}
+		fmt.Println("此段无论是否panic都会执行")
+	}()
+
+	func() {
+		fmt.Println("panic准备")
+		panic("too deep")
+	}()
+
+	fmt.Println("此段不会执行")
+}
+
+func B() {
+	defer func() {
+		p := recover()
+		fmt.Printf("recover得到的是: %#v ,静态类型: %T\n", p, p)
+
+		// 先做类型断言，再用 error 的方法
+		if err, ok := p.(error); ok {
+			fmt.Println(err.Error())
+			return
+		}else {
+			fmt.Println(p)
+		}
+		// 可以像正常if加东西，判断是哪个就走哪个，常见可以是字符串
+	}()
+
+	panic(errors.New("there is an error"))
+}
+
+func main(){
+	A()
+	B()
+}
