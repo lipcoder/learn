d269df980fe29c42cbf452dabfcb320424e819d4
lipcoder <lipcoder@foxmail.com>
2026-02-07 09:46:04 +0800

    goroutine

---
+++ go/2026.2.7.goroutine/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func A(a string) int {
+	for i := 0; i < 50; i++ {
+		fmt.Print(a)
+	}
+	return 1
+}
+
+func useA(){
+	go A("a")
+	go A("b")
+	// a := go A() 这个是无效的，go语句不能使用返回值
+	// 因为在尝试使用它之前，不能保证返回值已经准备好
+	time.Sleep(time.Second)
+}
+
+func main() {
+	useA()
+}
