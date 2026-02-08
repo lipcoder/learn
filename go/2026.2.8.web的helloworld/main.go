25c1cc3ac3fe52d769f07f6d86c4eeea4de10ba8
lipcoder <lipcoder@foxmail.com>
2026-02-08 16:54:09 +0800

    粗浅的web

---
+++ go/2026.2.8.web的helloworld/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func viewHandler(write http.ResponseWriter, request *http.Request) {
+	message := []byte("hello world")
+	_, err := write.Write(message)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func write(writer http.ResponseWriter, message string) {
+	_, err := writer.Write([]byte(message))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func d(writer http.ResponseWriter, request *http.Request) {
+	write(writer, "z")
+}
+func e(writer http.ResponseWriter, request *http.Request) {
+	write(writer, "x")
+}
+func f(writer http.ResponseWriter, request *http.Request) {
+	write(writer, "y")
+}
+
+func main() {
+	http.HandleFunc("/hello", viewHandler)
+	http.HandleFunc("/a", f)
+	http.HandleFunc("/b", d)
+	http.HandleFunc("/c", e)
+	fmt.Println("http://localhost:8080")
+	err := http.ListenAndServe("localhost:8080", nil)
+	log.Fatal(err)
+}
