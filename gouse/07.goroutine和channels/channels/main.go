1c7c8cfaafc653fb67a9549626e54f2a5378b0b4
lipcoder <lipcoder@foxmail.com>
2026-04-09 11:42:23 +0800

    channels

---
+++ gouse/07.goroutine和channels/channels/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func counter(out chan<- int) {
+	for x := 0; x < 100; x++ {
+		out <- x
+	}
+	close(out)
+}
+
+func squarer(out chan<- int, in <-chan int) {
+	for v := range in {
+		out <- v * v
+	}
+	close(out)
+}
+
+func printer(in <-chan int) {
+	for v := range in {
+		fmt.Println(v)
+	}
+}
+
+func main() {
+	naturals := make(chan int)
+	squares := make(chan int)
+	go counter(naturals) //调用counter（naturals）时，naturals的类型将隐式地从chan int转换成chan<- int
+	go squarer(squares, naturals)
+	printer(squares)
+}
